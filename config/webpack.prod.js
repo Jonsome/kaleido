72d170e9c4d400772af953c6fbd26ed6cef757d0
Jonsir <zs.flying@gmail.com>
2020-04-15 00:33:06 +0800

    split

--- config/webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const merge = require('webpack-merge')
-
-const commonWebpackConfig = require('./webpack.common')
-
-const prodWebpackConfig = {
-  mode: 'production',
-}
-
-module.exports = merge(commonWebpackConfig, prodWebpackConfig)
