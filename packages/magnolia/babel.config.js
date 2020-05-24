ca977e49166b27d40b697a4d09a4d65fd00e14a5
Jonsir <zs.flying@gmail.com>
2020-05-24 22:57:50 +0800

    update

--- packages/magnolia/babel.config.js
+++ packages/magnolia/babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
   plugins: [    
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-transform-runtime'
+    '@babel/plugin-transform-runtime',
+    'babel-plugin-macros'
   ]
 }
